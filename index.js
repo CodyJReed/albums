74d8ad7a3059973123060230ab7c9ef3b238c53d
Cody Reed <cody2j2reed@gmail.com>
2018-10-13 23:29:15 -0400

    Initial Commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { AppRegistry, Text } from "react-native";
 import React from "react";
 
 const App = () => {
-  return <Text>Some Text</Text>;
+  return <Text>Some Info</Text>;
 };
 
 AppRegistry.registerComponent("albums", () => App);
